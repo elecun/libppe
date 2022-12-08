d58195ed022a4c3e80ad0a54248599f7bdd55f20
elecun <elecun2@gmail.com>
2022-12-08 19:25:10 +0900

    add new library

---
+++ libfm/libfm.cc
@@ -0,0 +1,9 @@
+
+
+#include "libfm.hpp"
+
+namespace libfm {
+
+   
+
+}
---
+++ libfm/libfm.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file libfm.hpp
+ * @author Byunghun Hwang (bh.hwang@iae.re.kr)
+ * @brief M3-FS Focus Module
+ * @version 0.1
+ * @date 2022-12-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef _LIB_FM_HPP_
+#define _LIB_FM_HPP_
+
+
+#include <string>
+
+using namespace std;
+
+namespace libfm {
+
+
+
+} /* namespace */
+
+# endif
