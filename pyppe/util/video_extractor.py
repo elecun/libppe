8012809f9ba1807d90fbab8a5120d9837fa82fbf
elecun <elecun2@gmail.com>
2023-01-25 10:54:26 +0900

    added video extractor & samples

---
+++ pyppe/util/video_extractor.py
@@ -0,0 +1,40 @@
+'''
+Image file extractor from video file
+'''
+
+import cv2
+import argparse
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--video', nargs='?', required=True, help="input video file to extract images")
+    args = parser.parse_args()
+
+    if args.video is not None:
+        print(args.video)
+        _path, _ext = os.path.splitext(args.video)
+        frame_count = 0
+        if _ext == '.avi':
+            _video = cv2.VideoCapture(args.video)
+            _width  = int(_video.get(cv2.CAP_PROP_FRAME_WIDTH))
+            _height = int(_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            _fps = _video.get(cv2.CAP_PROP_FPS)
+            _frames = int(_video.get(cv2.CAP_PROP_FRAME_COUNT))
+            print("> Video source info. : ({},{}@{}), {} frames".format(_width, _height, _fps, _frames))
+
+            if _video.isOpened():
+                while True:
+                    ret, frame = _video.read()
+                    if ret == True:
+                        target_filename = _path+"_"+str(frame_count)+".png"
+                        cv2.imwrite(target_filename, frame)
+                        frame_count+=1
+                        cv2.imshow("image", frame)
+                    else:
+                        break
+                    key = cv2.waitKey(1)
+                    if key == 27:
+                        cv2.destroyAllWindows()
+                        break
+        print("finished")
