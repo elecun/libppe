cfafdc5ba1a1ef72333f3da16e011908516b913f
elecun <elecun2@gmail.com>
2023-04-24 12:27:08 +0900

    updated

--- bin/ppe_module.py
+++ bin/ppe_module.py
@@ -7,6 +7,7 @@ import numpy as np
 import os.path
 import torch
 import torchvision
+import math
 
 
 '''
@@ -19,11 +20,21 @@ def estimate(json_camera_param, json_job_desc):
         job = json.loads(json_job_desc)
         param = json.loads(json_camera_param)
         
+        # read pre-defined image info
+        if "resolution" in param:
+            _w, _h = param["resolution"]
+        else:
+            raise ValueError("Image resultion configurations are not defined")
+        
         # set camera parameter
         intrinsic_mtx = np.matrix([[float(param['fx']), 0.000000, float(param['cx'])], [0.000000, float(param['fy']), float(param['cy'])], [0.000000, 0.000000, 1.000000]])
         distorsion_mtx = np.matrix([[float(param['coeff_k1']), float(param['coeff_k2']), float(param['coeff_p1']), float(param['coeff_p2']), 0.]])
+        newcamera_mtx, roi = cv2.getOptimalNewCameraMatrix(intrinsic_mtx, distorsion_mtx,(_w, _h),0,(_w, _h))
         
-        #print(param['coord'][1])
+        # marker
+        markerdict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
+        markerparams = cv2.aruco.DetectorParameters_create()
+        markerparams.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
         
         # camera coord. / transformation matrix
         coord = np.matrix([[1,0,0,float(param['coord'][0])],[0,1,0,float(param['coord'][1])],[0,0,1,float(param['coord'][2])],[0,0,0,1]])
@@ -33,10 +44,31 @@ def estimate(json_camera_param, json_job_desc):
         if "files" in job and "path" in job:
             for job_file in job["files"]:
                 job_file_path = job["path"]+job_file
-                print(type(job_file))
                 
                 # estimation processing
                 if os.path.isfile(job_file_path): # if file exist
+                    
+                    # getting image profile
+                    raw_image = cv2.imread(job_file_path, cv2.IMREAD_UNCHANGED)
+                    raw_image_gray = cv2.cvtColor(raw_image, cv2.COLOR_BGR2GRAY)
+                    _raw_h, _raw_w, _raw_c = raw_image.shape
+                    if _raw_h!=_h and _raw_w!=_w:
+                        raise ValueError("Image shaoe is different from your configurations")
+                    
+                    # undistorsion
+                    ud_image_gray = cv2.undistort(raw_image_gray, intrinsic_mtx, None, newcamera_mtx)
+                    
+                    # preprocessing (binarization)
+                    ud_image_gray = cv2.bitwise_not(ud_image_gray)
+                    _, ud_image_binary = cv2.threshold(ud_image_gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+                    
+                    
+                    
+                    # find markers
+                    corners, ids, rejected = cv2.aruco.detectMarkers(ud_image_binary, markerdict, parameters=markerparams)
+                    print("marker detected")
+                    
+                    
                     p_dic = {}
                     p_dic["wafer_x"] = 0.0
                     p_dic["wafer_x"] = 0.0
@@ -60,5 +92,7 @@ def estimate(json_camera_param, json_job_desc):
         
     except json.decoder.JSONDecodeError :
         print("Decoding Job Description has failed")
+    except ValueError as e:
+        print(e)
     
     return json_result
